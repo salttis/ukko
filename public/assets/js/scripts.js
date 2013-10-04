9455d2287b4884a1ffbb6d711cfcfb1026732845
Juri Saltbacka <juri.saltbacka@gmail.com>
2013-10-04 18:21:23 +0300

    Package.json changes

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * your-project-name
+ * Your project name
+ * http://url.to/your-project
+ * @author Your name
+ * @version 1.0.0
+ * Copyright 2013. Your license licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    console.log('Perkele!');
+  });
+
+})(jQuery, window, document);
